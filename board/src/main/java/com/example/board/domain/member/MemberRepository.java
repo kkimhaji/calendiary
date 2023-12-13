68910fcff7d6e8a7237c7afaf98845ca1e877e60
kkimhaji <kkim.haji@gmail.com>
2023-12-13 23:21:26 +0900

    [fix] change P, A in MemberRepository

--- board/src/main/java/com/example/board/domain/member/MemberRepository.java
+++ board/src/main/java/com/example/board/domain/member/MemberRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Long, Member> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     @Override
-    Optional<Long> findById(Member member);
+    Optional<Member> findById(Long memberId);
 }
