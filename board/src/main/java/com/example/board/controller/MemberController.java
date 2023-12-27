8653d7f5ed28e0861bdf5f91a9efc7bc75f7d52a
kkimhaji <kkim.haji@gmail.com>
2023-12-28 01:03:14 +0900

    [feat] add to controller for login function

--- board/src/main/java/com/example/board/controller/MemberController.java
+++ board/src/main/java/com/example/board/controller/MemberController.java
@@ -1,25 +1,28 @@
 package com.example.board.controller;
 
 import com.example.board.domain.member.MemberRepository;
+import com.example.board.dto.TokenDto;
 import com.example.board.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class MemberController {
 
-    private final MemberRepository memberRepository;
     private final MemberService memberService;
 
     @PutMapping("/change/nickname")
     public String changeNickName(HttpServletRequest request, @RequestParam String newName){
         return memberService.changeName(request, newName);
     }
+
+    @PostMapping("/login")
+    public TokenDto login(@RequestParam String email, @RequestParam String password){
+        return memberService.login(email, password);
+    }
+
 
 }
