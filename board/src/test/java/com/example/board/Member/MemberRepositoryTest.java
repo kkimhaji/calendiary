23a351671873b5eec35dcc9062526ba4ca184871
kkimhaji <kkim.haji@gmail.com>
2024-12-05 19:33:45 +0900

    [test] set for test & member create, delete test

---
+++ board/src/test/java/com/example/board/Member/MemberRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.example.board.Member;
+
+import com.example.board.domain.member.Member;
+import com.example.board.domain.member.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.hamcrest.Matchers.nullValue;
+
+@DataJpaTest
+@AutoConfigureTestDatabase
+public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("멤버 생성 테스트")
+    void memberCreateTest(){
+        //given
+        Member member1 = Member.builder()
+                .email("aaa").password("123456").nickname("hello")
+                .build();
+        //when
+        Member result1 = memberRepository.save(member1);
+
+        //then
+        assertThat(result1.getEmail()).isEqualTo(member1.getEmail());
+    }
+
+    @Test
+    @DisplayName("멤버 삭제 테스트")
+    void memberDeleteTest(){
+        //given
+        Member member1 = Member.builder().email("aaa").password("123456").nickname("hello").build();
+        //when
+        Member result1 = memberRepository.save(member1);
+        memberRepository.delete(result1);
+
+        //then
+//        assertThatThrownBy(()->memberRepository.findById(member1.getMemberId())).isInstanceOf(IllegalArgumentException.class);
+        assertThat(memberRepository.findById(member1.getMemberId())).isEmpty();
+    }
+
+
+
+}
