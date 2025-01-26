07302ab44806f1f7397e469b057d361323eefb67
DESKTOP-MV6SI2A\hjh <kkim.haji@gmail.com>
2025-01-26 17:02:42 +0900

    [fix] enable jpa auditing for BaseEntity in Post

--- board/src/main/java/com/example/board/BoardApplication.java
+++ board/src/main/java/com/example/board/BoardApplication.java
@@ -3,7 +3,9 @@ package com.example.board;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = "com.example.board")
 public class BoardApplication {
     public static void main(String[] args) {
