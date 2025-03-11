6fce9b916d5bf8a8a2450813280b3b6e96b4f2bc
DESKTOP-MV6SI2A\hjh <kkim.haji@gmail.com>
2025-03-11 17:59:04 +0900

    [feat] update TeamRoleResponse for role description

--- board/src/main/java/com/example/board/dto/role/TeamRoleResponse.java
+++ board/src/main/java/com/example/board/dto/role/TeamRoleResponse.java
@@ -4,11 +4,12 @@ import com.example.board.permission.TeamPermission;
 import com.example.board.domain.role.TeamRole;
 import java.util.Set;
 
-public record TeamRoleResponse(Long id, String name, Set<TeamPermission> permissions, Long teamId) {
+public record TeamRoleResponse(Long id, String name, String description, Set<TeamPermission> permissions, Long teamId) {
     public static TeamRoleResponse from(TeamRole role) {
         return new TeamRoleResponse(
                 role.getId(),
                 role.getRoleName(),
+                role.getDescription(),
                 role.getPermissionSet(),
                 role.getTeam().getId()
         );
