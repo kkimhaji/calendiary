ebcb58c406f2c3f24e935bd9fa5e83c774bf10b6
DESKTOP-MV6SI2A\hjh <kkim.haji@gmail.com>
2025-03-19 00:27:42 +0900

    [fix] import HttpStatus

--- board/src/main/java/com/example/board/controller/AuthenticationController.java
+++ board/src/main/java/com/example/board/controller/AuthenticationController.java
@@ -12,6 +12,7 @@ import com.example.board.service.EmailService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
