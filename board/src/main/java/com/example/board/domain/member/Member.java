0ff895f20acbdcdde02c639dcb30c1e998ec50e4
DESKTOP-MV6SI2A\hjh <kkim.haji@gmail.com>
2025-05-22 22:57:43 +0900

    [refactor] remove unused code, Builder & AllArgsConstructor annotation in Entity class level

--- board/src/main/java/com/example/board/domain/member/Member.java
+++ board/src/main/java/com/example/board/domain/member/Member.java
@@ -7,9 +7,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,70 +23,6 @@ public class Member {
     private String verificationCode;
     private LocalDateTime verificationCodeExpiredAt;
 
-//    @OneToMany(mappedBy = "member")
-//    @JsonIgnore
-//    private Set<TeamMember> teamMemberships = new HashSet<>();
-
-
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        Set<GrantedAuthority> authorities = new HashSet<>();
-//
-//        // 각 팀별 역할에 따른 권한 추가
-//        teamMemberships.forEach(membership -> {
-//            // 팀별 역할 권한
-//            authorities.add(new SimpleGrantedAuthority(
-//                    String.format("TEAM_%d_ROLE_%s",
-//                            membership.getTeam().getTeamId(),
-//                            membership.getRole().getRoleName())
-//            ));
-//
-//            // 팀별 상세 권한
-//            String permissions = membership.getRole().getPermissions();
-//            for (TeamPermission permission : TeamPermission.values()) {
-//                if (PermissionUtils.hasPermission(permissions, permission)) {
-//                    authorities.add(new SimpleGrantedAuthority(
-//                            String.format("TEAM_%d_PERMISSION_%s",
-//                                    membership.getTeam().getTeamId(),
-//                                    permission.name())
-//                    ));
-//                }
-//            }
-//        });
-//
-//        return authorities;
-//    }
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return new ArrayList<>();
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return email;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
-
-
     public void setVerified(){
         this.enabled = true;
         this.verificationCode = null;
@@ -100,7 +34,6 @@ public class Member {
         this.verificationCodeExpiredAt = expiredAt;
     }
 
-
     public void updateName(String newNickname) {
         this.nickname = newNickname;
     }
@@ -108,5 +41,17 @@ public class Member {
     public void updatePassword(String newPassword)
     {
         this.password = newPassword;
+    }
+
+    //builder대신 다른 메서드로 대체할 것
+    @Builder
+    public Member(Long memberId, String email, String nickname, String password, boolean enabled, String verificationCode, LocalDateTime verificationCodeExpiredAt){
+        this.memberId = memberId;
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+        this.enabled = enabled;
+        this.verificationCode = verificationCode;
+        this.verificationCodeExpiredAt = verificationCodeExpiredAt;
     }
 }
