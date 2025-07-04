f3e41c768caba5c5fed3d4f3cf86549865eeea3b
kkimhaji <kkim.haji@gmail.com>
2025-07-04 21:51:13 +0900

    [test] udpate category test

--- board/src/test/java/com/example/board/category/CategoryServiceTest.java
+++ board/src/test/java/com/example/board/category/CategoryServiceTest.java
@@ -1,5 +1,8 @@
 package com.example.board.category;
 
+import com.example.board.category.dto.CategoryRolePermissionDTO;
+import com.example.board.category.dto.CategoryRolePermissionResponse;
+import com.example.board.category.dto.UpdateCategoryRequest;
 import com.example.board.role.TeamRole;
 import com.example.board.support.AbstractTestSupport;
 import com.example.board.support.TestDataBuilder;
@@ -11,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 
 import static com.example.board.permission.CategoryPermission.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,6 +46,25 @@ public class CategoryServiceTest extends AbstractTestSupport {
         assertThat(categoryService.checkCategoryPermission(category.getId(), member1, CREATE_COMMENT)).isTrue();
         assertThat(categoryService.checkCategoryPermission(category.getId(), member2, CREATE_COMMENT)).isFalse();
         assertThat(categoryService.checkCategoryPermission(category.getId(), member2, VIEW_POST)).isTrue();
+    }
 
+    @Test
+    void updateCategoryTest() {
+        String updateName = "updated category";
+        String updateDesc = "update category test";
+        var dto = new CategoryRolePermissionDTO(teamRole.getId(), new HashSet<>(List.of(CREATE_POST)));
+        var request = new UpdateCategoryRequest(updateName, updateDesc, Optional.of(List.of(dto)));
+        var updatedCategory = categoryService.updateCategory(team.getId(), category.getId(), request);
+
+        assertThat(updatedCategory.name()).isEqualTo(updateName);
+        assertThat(updatedCategory.description()).isEqualTo(updateDesc);
+        assertThat(updatedCategory.id()).isEqualTo(category.getId());
+        assertThat(updatedCategory.rolePermissions()).hasSize(1);
+
+        CategoryRolePermissionResponse rolePermission = updatedCategory.rolePermissions().get(0);
+
+        assertThat(rolePermission.roleId()).isEqualTo(teamRole.getId());
+        assertThat(rolePermission.roleName()).isEqualTo(teamRole.getRoleName());
+        assertThat(rolePermission.permissions()).contains(CREATE_POST);
     }
 }
