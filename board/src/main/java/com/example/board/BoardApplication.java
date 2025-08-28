55b38e8d1b0eb3b34345f9d70b58c3e0476b33be
kkimhaji <kkim.haji@gmail.com>
2025-08-28 23:56:31 +0900

    [refactor] remove unused imports in controller classes

--- board/src/main/java/com/example/board/BoardApplication.java
+++ board/src/main/java/com/example/board/BoardApplication.java
@@ -1,6 +1,5 @@
 package com.example.board;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
